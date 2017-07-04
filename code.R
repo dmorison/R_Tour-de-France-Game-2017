30ee942e639b8ecf621ce3341bc41115cece0f44
David Morison <davemorison@gmail.com>
2017-07-04 07:20:27 +0100

    the grand depart has taken place

--- code.R
+++ code.R
@@ -1,17 +1,22 @@
 setwd("C:/Users/davem/coding_2017/tourdefrance_game")
+df_start_list <- read.csv("start-list_2017.csv")
 ### SECTION TO UPDATE - FOLLOW STEPS ###
 ### update stage with which the results are going to be for
 stage <- "Stage_3"
-### update stage rider code results: win, yellow, green, polka, white, red, team
-st_rider_code_results <- c("num_117", "num_099", "num_057", "num_009", "num_123", "num_111", "BOR")
-### update stage rider name results: win, yellow, green, polka, white, red, team
-st_rider_name_results <- c("SINKELDAM Ramon", "Chris Froome", "HALLER Marco", "SANCHEZ GIL Luis-Leon",
-                           "LADAGNOUS Matthieu", "LANGEVELD Sebastian", "Astana")
+### Look at previous stage general classification to determine who the jersey wearers were for the stage to update
+df_start_list[grep("Yoan", df_start_list$Rider), ]
+### update stage rider name results: win, team, yellow, green, polka, white, red
+st_rider_name_results <- c("Peter Sagan (Svk)", "Bore-Hansgrohe", "Geraint Thomas (GBr)", "Marcel Kittel (Ger)",
+                           "Taylor Phinney (USA)", "Stefan Kung (Sui)", "Yoann Offredo (Fra)")
+### update stage rider code results: win, team, yellow, green, polka, white, red
+st_rider_code_results <- c("rd131", "BOR", "rd008", "rd181",
+                           "rd086", "rd093", "rd211")
 
 ########################################
+### DO FIRST STAGE MANUALLY SO SKIP THIS STEP AND GO TO CALCULATION !!!
 df_rider_code_results <- read.csv("rider-code-results.csv", header = TRUE)
 df_rider_name_results <- read.csv("rider-name-results.csv", header = TRUE)
-### DO FIRST STAGE MANUALLY SO SKIP THIS STEP AND GO TO CALCULATION !!!
+
 row_rider_code_results <- c(stage, st_rider_code_results)
 df_rider_code_results[] <- lapply(df_rider_code_results, as.character)
 update_df_rider_code_results <- rbind(row_rider_code_results, df_rider_code_results)
@@ -27,8 +32,8 @@ df_players <- read.csv("players-codes.csv")
 df_players_results <- read.csv("players-stage-results.csv")
 ### REMEMBER TO SKIP THIS FOR FIRST STAGE!!!
 df_player_st_winners <- read.csv("player-st-winners.csv")
-
-scores <- c(3, 2, 1, 1, 1, 1, 1)
+###
+scores <- c(3, 1, 2, 1, 1, 1, 1)
 st_results <- apply(df_players[, 2:8], 1, function(x){ sum(scores[which(st_rider_code_results %in% x)]) })
 st_results_df <- cbind(df_players_results, st_results)
 
@@ -38,7 +43,7 @@ if (length(st_player_winner) > 1) {
   st_player_winner <- paste(st_player_winner, collapse = ', ')
 }
 row_st_player_winner <- c(stage, st_player_winner, st_player_topscore)
-### REMEMBER TO ADD ABOVE MANUALLY FOR FIRST STAGE AND THEN SKIP THE NEXT TWO LINES!!!
+### REMEMBER TO ADD ABOVE MANUALLY FOR FIRST STAGE AND THEN SKIP THE NEXT THREE LINES!!!
 df_player_st_winners[] <- lapply(df_player_st_winners, as.character)
 update_df_player_st_winners <- rbind(row_st_player_winner, df_player_st_winners)
 write.csv(update_df_player_st_winners, file = "player-st-winners.csv", row.names = FALSE)
@@ -54,27 +59,26 @@ if(ncol(st_results_df) > 2) {
 player_totals_df <- data.frame(Players = st_results_df$Players, Total_score = player_totals)
 player_totals_df <- player_totals_df[order(-player_totals_df$Total_score), ]
 write.csv(player_totals_df, file = "players-leaderboard.csv", row.names = FALSE)
+
+players_stage_accum <- read.csv("players-accum-stage-results.csv")
+st_accum_df <- cbind(players_stage_accum, player_totals)
+colnames(st_accum_df)[ncol(st_accum_df)] <- stage
+write.csv(st_accum_df, file = "players-accum-stage-results.csv", row.names = FALSE)
 ########################################
 
 ### CONSTANTS - NO NEED TO TOUCH THIS CODE AFTER INITIALIZING IT IN BEGINNING
 # scoring <- c("Stage_win", "Yellow", "Green", "Polka_dot", "White", "Agressive", "Team")
-### Create players code table in beginning
-player_1 <- c("Dave", "MVS", "num_001",	"num_002", "num_117",	"num_045", "num_123",	"num_074")
-player_2 <- c("Jayde", "AST", "num_001", "num_006", "num_099", "num_023", "num_056", "num_143")
-player_3 <- c("Storm", "AST", "num_117", "num_009", "num_098", "num_086", "num_057", "num_143")
-
-players <- data.frame(rbind(player_1, player_2, player_3))
-colnames(players) <- c("Player", "Team", "Rider_1", "Rider_2", "Rider_3", "Rider_4", "Rider_5", "Rider_6")
-write.csv(players, file = "players-codes.csv", row.names = FALSE)
-
-### Create players name table in beginning
-
-
-### Create players stage results table in beginning
-players_stage_results <- data.frame(players$Player)
+### Players teams and rider names
+df_players_teams <- read.csv("players-teams.csv")
+### Complete start list of teams and riders
+df_start_list <- read.csv("start-list_2017.csv")
+### First read in df_players table from above then create players stage results table in beginning
+players_stage_results <- data.frame(df_players$Player)
 colnames(players_stage_results) <- "Players"
 write.csv(players_stage_results, file = "players-stage-results.csv", row.names = FALSE)
-
+### Initialise the players accumulative stage scores table (used for progress line chart)
+players_stage_accum <- data.frame(Players = df_players$Players, Stage_0 = rep(0, length(df_players$Players)))
+write.csv(players_stage_accum, file = "players-accum-stage-results.csv", row.names = FALSE)
 ##################
 ### DO NOT USE ###
 ### Create initial df_player_st_winners table
